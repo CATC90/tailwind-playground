8041cfaea2a0246182b97e4ba77fbf811cb945c5
Carlos Trujillo <cacuevas@stefanini.com>
2021-11-14 12:21:46 -0300

    :tada: tailwind
    initial table

---
+++ components/Search.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { SearchIcon } from "@heroicons/react/outline";
+
+const Search = () => {
+  return (
+    <div className="border rounded focus:shadow-outline shadow flex items-center">
+      <input
+        class="bg-transparent appearance-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+        id="username"
+        type="text"
+        placeholder="Buscar"
+      />
+      <SearchIcon className="h-5 w-5 mx-2 text-gray-700" />
+    </div>
+  );
+};
+
+export { Search };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      nunito: ["nunito", "sans-serif"],
+      roboto: ['"Roboto"', "serif"], // Ensure fonts with spaces have " " surrounding it.
+    },
+    extend: {
+      "min-width": {
+        "table-min-width": "774px",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
